dd2c774ec2e93969addf3f1d32e602ab465658da
cbassuser <cbassuser@cbaezar.(none)>
2015-03-30 14:19:10 +0200

    added grabImage.py

---
+++ grabImage.py
@@ -0,0 +1,36 @@
+#test script for grabbing images for the optical pointing of the antenna
+# Revision 1 : 30/3/2015
+# Charles Copley
+
+
+import cv2 #for ubuntu 12.04 install see: http://karytech.blogspot.com/2012/05/opencv-24-on-ubuntu-1204.html
+import cv
+import numpy as np
+import time
+import sys, optparse
+import pdb
+import matplotlib.pylab as plt
+import os
+
+#which device (i.e. /dev/video0) should we use?
+CAMERA_DEVICE_INDEX=0
+
+
+cam0t=cv2.VideoCapture(CAMERA_DEVICE_INDEX)
+#set up v4l2 to the correct input on the frame grabber
+os.system("v4l2-ctl -d /dev/video0 -i 0 --set-fmt-video=width=768,height=480")
+time.sleep(1)
+os.system("v4l2-ctl -d /dev/video0 -i 1 --set-fmt-video=width=768,height=480")
+time.sleep(1)
+#and set up the correct width
+width =768
+height = 480
+cam0t.set(3,width)
+cam0t.set(4,height)
+#read in the image from the frame grabber
+ret,frame = cam0t.read()
+cam0t.release()
+cv2.imshow("Test",frame)
+cv2.imwrite('file.png',frame)
+print frame
+cv2.waitKey(0)
